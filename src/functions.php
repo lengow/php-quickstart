f72001b8239f582a19295a83b6ea4b3bf1e2843e
Mathieu <mathieu.sabourin@lengow.com>
2016-04-13 18:10:59 +0200

    lengow fork zuora lib

--- src/functions.php
+++ src/functions.php
@@ -1,20 +1,20 @@
 <?php
 /*
  *    Copyright (c) 2010 Zuora, Inc.
- *    
- *    Permission is hereby granted, free of charge, to any person obtaining a copy of 
- *    this software and associated documentation files (the "Software"), to use copy, 
- *    modify, merge, publish the Software and to distribute, and sublicense copies of 
+ *
+ *    Permission is hereby granted, free of charge, to any person obtaining a copy of
+ *    this software and associated documentation files (the "Software"), to use copy,
+ *    modify, merge, publish the Software and to distribute, and sublicense copies of
  *    the Software, provided no fee is charged for the Software.  In addition the
  *    rights specified above are conditioned upon the following:
- *    
+ *
  *    The above copyright notice and this permission notice shall be included in all
  *    copies or substantial portions of the Software.
- *    
+ *
  *    Zuora, Inc. or any other trademarks of Zuora, Inc.  may not be used to endorse
  *    or promote products derived from this Software without specific prior written
  *    permission from Zuora, Inc.
- *    
+ *
  *    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *    FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL
@@ -26,7 +26,7 @@
  *    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 # method to query all records of a type.
-function queryAll($instance, $query){
+function zuoraQueryAll($instance, $query){
 
     $moreCount = 0;
     $recordsArray = array();
@@ -51,12 +51,12 @@ function queryAll($instance, $query){
         $recordsArray = array_merge($recordsArray, $newRecords);
 
         while (!$done && $locator && $moreCount == 0){
-        
+
             $start = time();
             $result = $instance->queryMore($locator);
             $end = time();
             $elapsed = $end - $start;
-    
+
             $done = $result->result->done;
             $size = $result->result->size;
             $locator = $result->result->queryLocator;
@@ -69,7 +69,7 @@ function queryAll($instance, $query){
             } else {
                 $recordsArray = array_merge($recordsArray, $newRecords);
             }
-    
+
         }
     }
 
@@ -80,25 +80,25 @@ function queryAll($instance, $query){
 
 }
 
-function getPostValue($name,$default=null){
-	$v = $_POST[$name];	
-	if(!isset($v)){	
+function zuoraGetPostValue($name,$default=null){
+	$v = $_POST[$name];
+	if(!isset($v)){
 		$v = $default;
 	}
 	return $v;
 }
-function isEmpty($var){
+function zuoraIsEmpty($var){
 	return !isset($var) or trim($var)=='';
 }
 
-function makeAccount($name,$currency,$status){
+function zuoraMakeAccount($name,$currency,$status){
     $zAccount = new Zuora_Account();
     $zAccount->AllowInvoiceEdit = 1;
     $zAccount->AutoPay = 0;
     $zAccount->Batch = 'Batch1';
     $zAccount->BillCycleDay = 1;
     $zAccount->Currency = $currency;
-    $zAccount->Name = $name;    
+    $zAccount->Name = $name;
     $zAccount->PaymentTerm = 'Due Upon Receipt';
     $zAccount->Status = $status;
 
@@ -106,28 +106,28 @@ function makeAccount($name,$currency,$status){
 		//$zAccount->PurchaseOrderNumber = 'PO-1223471249003';
 		return $zAccount;
 }
-function makeContact($firstName,$lastName,$address1,$address2,$city,$state,$country,$postalCode,$workMail,$workPhone,$accountId=null){
-	
+function zuoraMakeContact($firstName,$lastName,$address1,$address2,$city,$state,$country,$postalCode,$workMail,$workPhone,$accountId=null){
+
 	  $zBillToContact = new Zuora_Contact();
-    
+
     $zBillToContact->FirstName = $firstName;
-    $zBillToContact->LastName = $lastName;    
+    $zBillToContact->LastName = $lastName;
     $zBillToContact->Address1 = $address1;
     $zBillToContact->Address2 = $address2;
     $zBillToContact->City = $city;
     $zBillToContact->State = $state;
-    $zBillToContact->Country = $country;    
+    $zBillToContact->Country = $country;
     $zBillToContact->PostalCode = $postalCode;
     $zBillToContact->WorkEmail = $workMail;
     $zBillToContact->WorkPhone = $workPhone;
  		$zBillToContact->AccountId = $accountId;
- 		
+
 		return  $zBillToContact;
 }
-function makePaymentMethod($creditCardHolderName, $address1,$address2, $city, $state, $country, $postalCode, $creditCardType, $creditCardNumber, $creditCardExpirationMonth, $creditCardExpirationYear,$accountId=null){
+function zuoraMakePaymentMethod($creditCardHolderName, $address1,$address2, $city, $state, $country, $postalCode, $creditCardType, $creditCardNumber, $creditCardExpirationMonth, $creditCardExpirationYear,$accountId=null){
 	  $zPaymentMethod = new Zuora_PaymentMethod();
     $zPaymentMethod->AccountId = $accountId;
-    
+
     $zPaymentMethod->CreditCardAddress1 = $address1;
     $zPaymentMethod->CreditCardAddress2 = $address2;
     $zPaymentMethod->CreditCardCity = $city;
@@ -139,64 +139,63 @@ function makePaymentMethod($creditCardHolderName, $address1,$address2, $city, $s
     $zPaymentMethod->CreditCardPostalCode = $postalCode;
     $zPaymentMethod->CreditCardState = $state;
     $zPaymentMethod->CreditCardType = $creditCardType;
-    
-    $zPaymentMethod->Type = 'CreditCard';	
+
+    $zPaymentMethod->Type = 'CreditCard';
     return $zPaymentMethod;
 }
-function makeSubscription($subscriptionName, $subscriptionNotes){
+function zuoraMakeSubscription($subscriptionName, $subscriptionNotes){
 	  $date = date('Y-m-d\TH:i:s',time());
-	  
+
 	  $zSubscription = new Zuora_Subscription();
 
     $zSubscription->Name = $subscriptionName;
 		$zSubscription->Notes = $subscriptionNotes;
-		
+
     $zSubscription->ContractAcceptanceDate = $date;
     $zSubscription->ContractEffectiveDate = $date;
-   
+
     $zSubscription->InitialTerm = 12;
     $zSubscription->RenewalTerm = 12;
     $zSubscription->ServiceActivationDate = $date;
-   
+
     $zSubscription->TermStartDate=$date;
 		$zSubscription->Status = 'Active';
 		$zSubscription->Currency = 'USD';
 		$zSubscription->AutoRenew = 1;
-		
+
 		return 	$zSubscription;
 }
-function setRatePlanData($zSubscriptionData,$chargeIds,$rateplancharges,$productRatePlanId){
+function zuoraSetRatePlanData($zSubscriptionData,$chargeIds,$rateplancharges,$productRatePlanId){
 	  $zRatePlan = new Zuora_RatePlan();
     $zRatePlan->AmendmentType = 'NewProduct';
 
     $zRatePlan->ProductRatePlanId = $productRatePlanId;
     $zRatePlanData = new Zuora_RatePlanData($zRatePlan);
-    
+
     foreach($chargeIds as $cid){
     	foreach($rateplancharges as $rc){
     		if($rc->Id == $cid){
 		 			$rpc = new Zuora_RatePlanCharge();
 			    $rpc->ProductRatePlanChargeId = $cid;
-			    if($rc->DefaultQuantity>0){			    
+			    if($rc->DefaultQuantity>0){
 			    	$rpc->Quantity =  1;
 			  	}
 			    $rpc->TriggerEvent ="ServiceActivation";
-			    
+
 			    $zPlanChargeData = new Zuora_RatePlanChargeData($rpc);
-			    
-			    
-			    $zRatePlanData->addRatePlanChargeData($zPlanChargeData);    			
+
+
+			    $zRatePlanData->addRatePlanChargeData($zPlanChargeData);
     		}
     	}
     }
-    
+
   $zSubscriptionData->addRatePlanData($zRatePlanData);
-    
+
 }
 
-function makeSubscriptionData($subscription,$chargeIds,$rateplancharges,$rateplanId){
+function zuoraMakeSubscriptionData($subscription,$chargeIds,$rateplancharges,$rateplanId){
 	 $zSubscriptionData = new Zuora_SubscriptionData($subscription);
    setRatePlanData($zSubscriptionData,$chargeIds,$rateplancharges,$rateplanId);
 	 return $zSubscriptionData;
-}	
-?>
+}
